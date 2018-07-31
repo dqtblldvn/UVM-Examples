8b3d2a25e7919cd56e36894b2dd2c673c16e2bd3
Thinh Do <30658664+dqtblldvn@users.noreply.github.com>
2018-07-31 16:33:14 +0700

    update top.sv

--- Counter/top.sv
+++ Counter/top.sv
@@ -8,9 +8,7 @@ module top;
 	Counter_bfm bfm();
 	Counter DUT(.clk(bfm.clk),.datain(bfm.datain),.rst(bfm.rst),.en(bfm.en),
 		.load(bfm.load),.updn(bfm.updn),.dataout(bfm.dataout));
-	bind DUT Counter_assertion CounterDUT_assertion(.clk(DUT.clk),.datain(DUT.datain),.rst(DUT.rst),.en(DUT.en),
-		.load(DUT.load),.updn(DUT.updn),.dataout(DUT.dataout));
-
+	
 	initial begin
 		uvm_config_db #(virtual Counter_bfm)::set(null,"*","bfm",bfm);
 		run_test();
